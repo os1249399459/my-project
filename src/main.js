5cad929c494c1bde81c800cd8313801eb6873210
李超 <1249399459@qq.com>
2020-12-11 10:31:24 +0800

    re organize

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import router from './router'
-import App from './components/Login.vue'
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
 
 new Vue({
-  router,
-  render: h => h(App)
+  router
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import VueRouter from "vue-router";
-import App from "../components/Login.vue";
-import HelloWorld from "../components/HelloWorld.vue";
+import ElementUI from 'element-ui'
+import App from "../views/Login.vue";
+import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
+Vue.use(ElementUI)
 
 const routes = [
   { path: "", component: App },
-  { path: "/home", component: HelloWorld },
+  { path: "/home", component: Home },
 ];
 
 export default new VueRouter({
